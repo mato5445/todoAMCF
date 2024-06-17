47a2acdb6d85757198d25b072236d32e87bd8e8d
martinrusnak365 <martin.rusnak@365.bank>
2024-06-17 07:52:35 +0200

    authorization

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,16 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, importProvidersFrom} from '@angular/core';
+import {provideRouter} from '@angular/router';
+import {routes} from './app.routes';
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
+import {AngularFireAuthModule} from "@angular/fire/compat/auth";
+import {AngularFireModule} from "@angular/fire/compat";
+import {AngularFireDatabaseModule} from "@angular/fire/compat/database";
+import {firebaseConfig} from "./constants/constants";
+import {provideHttpClient, withInterceptors} from "@angular/common/http";
+import {MatNativeDateModule} from "@angular/material/core";
 
-import { routes } from './app.routes';
+import {authInterceptor} from "./auth/interceptor/auth.interceptor";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideHttpClient(withInterceptors([authInterceptor])), provideRouter(routes), provideAnimationsAsync(), importProvidersFrom([AngularFireModule.initializeApp(firebaseConfig), AngularFireAuthModule, MatNativeDateModule, AngularFireDatabaseModule]), provideAnimationsAsync()]
 };
